00ec5a192f6c3a8ab20a62277a162db0482f88a5
Karl Jamoralin <kjamoralin@gitlab.com>
2024-12-20 11:20:59 -0800

    Add core telemetry objects

---
+++ src/main/kotlin/com/gitlab/eclipse/telemetry/TelemetryContext.kt
@@ -0,0 +1,14 @@
+package com.gitlab.eclipse.telemetry
+
+data class TelemetryContext(
+    val trackingId: String,
+    val optionId: Int? = null
+) {
+    override fun toString() = buildString {
+        append("{trackingId:'$trackingId'")
+
+        optionId?.let {
+            append(",optionId:$optionId")
+        }
+    }
+}
