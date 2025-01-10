55e93f7e613cb47517f8a8509fabf2b863fbc2ef
Jean-Gabriel Doyon <jgdoyon@gitlab.com>
2025-01-10 11:04:29 -0500

    Add missing part

--- src/main/kotlin/com/gitlab/eclipse/utils/Logger.kt
+++ src/main/kotlin/com/gitlab/eclipse/utils/Logger.kt
@@ -2,5 +2,6 @@ package com.gitlab.eclipse.utils
 
 import org.eclipse.core.runtime.ILog
 import org.eclipse.core.runtime.Platform
+import org.osgi.framework.FrameworkUtil
 
-inline fun <reified T> logger(): ILog = Platform.getLog(T::class.java)
+inline fun <reified T> logger(): ILog = Platform.getLog(FrameworkUtil.getBundle(T::class.java))
