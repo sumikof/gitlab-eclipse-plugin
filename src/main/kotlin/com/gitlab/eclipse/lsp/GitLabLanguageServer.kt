e280bb57fffe19a796e863046045415cdd6cc65b
Karl Jamoralin <21925675-kjamoralin@users.noreply.gitlab.com>
2025-01-15 21:43:43 +0000

    feat: Send telemetry events via LS

--- src/main/kotlin/com/gitlab/eclipse/lsp/GitLabLanguageServer.kt
+++ src/main/kotlin/com/gitlab/eclipse/lsp/GitLabLanguageServer.kt
@@ -1,6 +1,7 @@
 package com.gitlab.eclipse.lsp
 
 import com.gitlab.eclipse.lsp.plugins.messages.ExtensionToPluginNotification
+import com.gitlab.eclipse.telemetry.params.TelemetryParams
 import org.eclipse.lsp4j.jsonrpc.services.JsonNotification
 import org.eclipse.lsp4j.jsonrpc.services.JsonRequest
 import org.eclipse.lsp4j.services.LanguageServer
@@ -8,6 +9,9 @@ import org.eclipse.lsp4j.services.LanguageServer
 interface GitLabLanguageServer : LanguageServer {
   @JsonRequest("$/gitlab/webview-metadata")
   fun webviewMetadata(): java.util.concurrent.CompletableFuture<List<WebviewInfo?>?>?
+
+  @JsonNotification("$/gitlab/telemetry")
+  fun telemetry(params: TelemetryParams)
 
   @JsonNotification("$/gitlab/plugin/notification")
   fun pluginNotification(notification: ExtensionToPluginNotification)
--- src/main/kotlin/com/gitlab/eclipse/telemetry/TelemetryService.kt
+++ src/main/kotlin/com/gitlab/eclipse/telemetry/TelemetryService.kt
@@ -1,8 +1,61 @@
 package com.gitlab.eclipse.telemetry
 
-object TelemetryService {
-  // Returns the string version for now. In the future, this will send telemetry to LS.
-  fun track(action: TelemetryAction): String {
-    return action.toString()
+import com.gitlab.eclipse.lsp.GitLabLanguageServerWrapper
+import com.gitlab.eclipse.telemetry.params.TelemetryContext
+import com.gitlab.eclipse.telemetry.params.TelemetryParams
+
+class TelemetryService(
+  private val gitLabLanguageServerWrapper: GitLabLanguageServerWrapper
+) {
+
+  private val gitLabLanguageServer get() = gitLabLanguageServerWrapper.languageServer
+
+  // The following code suggestions telemetry can be handled by the language client (i.e. this Eclipse extension).
+  // The rest are handled by the language server for us.
+  // [Reference](https://gitlab.com/gitlab-org/editor-extensions/gitlab-lsp/-/blob/main/docs/telemetry.md)
+
+  fun sendCodeSuggestionAcceptedTelemetry(trackingId: String, optionId: Int) {
+    gitLabLanguageServer?.telemetry(
+      TelemetryParams(
+        action = "suggestion_accepted",
+        context = TelemetryContext(trackingId, optionId)
+      )
+    )
+  }
+
+  fun sendCodeSuggestionNotProvidedTelemetry(trackingId: String) {
+    gitLabLanguageServer?.telemetry(
+      TelemetryParams(
+        action = "suggestion_not_provided",
+        context = TelemetryContext(trackingId)
+      )
+    )
+  }
+
+  fun sendCodeSuggestionCancelledTelemetry(trackingId: String) {
+    gitLabLanguageServer?.telemetry(
+      TelemetryParams(
+        action = "suggestion_cancelled",
+        context = TelemetryContext(trackingId)
+      )
+    )
+  }
+
+  fun sendCodeSuggestionRejectedTelemetry(trackingId: String) {
+    gitLabLanguageServer?.telemetry(
+      TelemetryParams(
+        action = "suggestion_rejected",
+        context = TelemetryContext(trackingId)
+      )
+    )
+  }
+
+  fun sendCodeSuggestionShownTelemetry(trackingId: String) {
+    gitLabLanguageServer?.telemetry(
+      TelemetryParams(
+        action = "suggestion_shown",
+        context = TelemetryContext(trackingId)
+      )
+    )
   }
 }
