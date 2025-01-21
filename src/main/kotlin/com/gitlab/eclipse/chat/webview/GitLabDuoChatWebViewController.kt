99af3ba1c496b74ab06724cbce59dcdbb4fe4215
Jean-Gabriel Doyon <jgdoyon@gitlab.com>
2025-01-21 12:43:31 -0500

    feat: Support duo-chat-v2 showMessage notification

--- src/main/kotlin/com/gitlab/eclipse/chat/webview/GitLabDuoChatWebViewController.kt
+++ src/main/kotlin/com/gitlab/eclipse/chat/webview/GitLabDuoChatWebViewController.kt
@@ -3,15 +3,19 @@ package com.gitlab.eclipse.chat.webview
 import com.gitlab.eclipse.chat.context.CurrentFileContextProvider
 import com.gitlab.eclipse.lsp.FileContext
 import com.gitlab.eclipse.lsp.plugins.annotations.PluginController
+import com.gitlab.eclipse.lsp.plugins.annotations.PluginNotification
 import com.gitlab.eclipse.lsp.plugins.annotations.PluginRequest
 import com.gitlab.eclipse.utils.TextEditorProvider
 import com.gitlab.eclipse.utils.currentDisplay
+import com.gitlab.eclipse.utils.logger
 
 @PluginController("duo-chat-v2")
 class GitLabDuoChatWebViewController(
   private val textEditorProvider: TextEditorProvider = TextEditorProvider(),
   private val currentFileContextProvider: CurrentFileContextProvider = CurrentFileContextProvider()
 ) {
+  private val logger by lazy { logger<GitLabDuoChatWebViewController>() }
+
   @PluginRequest("getCurrentFileContext")
   fun getCurrentFileContext(): FileContext? {
     val textEditor = textEditorProvider.getActiveTextEditor()
@@ -19,6 +23,16 @@ class GitLabDuoChatWebViewController(
 
     return currentDisplay.syncCall<FileContext, Exception> {
       currentFileContextProvider.provide(textEditor)
+    }
+  }
+
+  @PluginNotification("showMessage")
+  fun showMessage(notification: ShowMessageNotification) {
+    when (val type = notification.type) {
+      "error" -> logger.error(notification.message)
+      "warning" -> logger.warn(notification.message)
+      "info" -> logger.warn(notification.message)
+      else -> logger.warn("Received unknown message type ($type) with content: ${notification.message}")
     }
   }
 }
---
+++ src/main/kotlin/com/gitlab/eclipse/chat/webview/ShowMessageNotification.kt
@@ -0,0 +1,6 @@
+package com.gitlab.eclipse.chat.webview
+
+data class ShowMessageNotification(
+  val type: String,
+  val message: String
+)
