b86f172f8cac83e5b8fcb19f8cdea5559c5c1085
Erran Carey <ecarey@gitlab.com>
2025-01-24 21:04:28 +0000

    feat: Add GitLab button to Eclipse trim

---
+++ src/main/kotlin/com/gitlab/eclipse/handlers/ShowSettings.kt
@@ -0,0 +1,26 @@
+package com.gitlab.eclipse.handlers
+
+import com.gitlab.eclipse.utils.logger
+import org.eclipse.core.commands.AbstractHandler
+import org.eclipse.core.commands.ExecutionEvent
+import org.eclipse.core.commands.ExecutionException
+import org.eclipse.ui.dialogs.PreferencesUtil
+import org.eclipse.ui.handlers.HandlerUtil
+
+@Suppress("unused")
+class ShowSettings : AbstractHandler() {
+  val logger = logger<ShowSettings>()
+
+  @Throws(ExecutionException::class)
+  override fun execute(event: ExecutionEvent): Any? {
+    logger.info("Showing GitLab for Eclipse settings.")
+    val shell = HandlerUtil.getActiveWorkbenchWindow(event)?.shell ?: return null
+    PreferencesUtil.createPreferenceDialogOn(
+      shell,
+      "com.gitlab.eclipse.preferences.GitLabPreferencePage",
+      emptyArray(),
+      null
+    ).open()
+    return null
+  }
+}
