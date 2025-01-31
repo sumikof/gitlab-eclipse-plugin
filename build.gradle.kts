0a2bf4fbde798d5817bc6104fff2f2b04a532a95
Jean-Gabriel Doyon <jgdoyon@gitlab.com>
2025-01-31 14:38:02 -0500

    chore: Increment version to 0.3.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,8 +36,8 @@ plugins {
 
 allprojects {
   group = "com.gitlab.eclipse"
-  version = "0.3.1-SNAPSHOT"
-  ext["bundleVersion"] = "0.3.1.${Instant.now().toEpochMilli()}"
+  version = "0.3.2-SNAPSHOT"
+  ext["bundleVersion"] = "0.3.2.${Instant.now().toEpochMilli()}"
 
   repositories {
     gradlePluginPortal()
