490b2826421f9ba37dd91f32e9080f14db5495a1
Laura Ionel <lionel@gitlab.com>
2025-02-11 12:53:34 +0100

    Introduce TokenProvider and PatProvider

---
+++ src/main/kotlin/com/gitlab/eclipse/lsp/authentication/PatProvider.kt
@@ -0,0 +1,9 @@
+package com.gitlab.eclipse.lsp.authentication
+
+import com.gitlab.eclipse.preferences.storage.SecretStorage
+
+class PatProvider : TokenProvider {
+  override fun getToken(): String {
+    return SecretStorage("gitlab.com").getSecret("personal_access_token").orEmpty()
+  }
+}
