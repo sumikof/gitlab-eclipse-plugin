697fc9ac6bfc1a38c4d2d19a8bb23ce33aedc227
Karl Jamoralin <21925675-kjamoralin@users.noreply.gitlab.com>
2025-05-23 14:54:20 -0700

    Update update_version.sh to also bump version for swtbot's MANIFEST.MF

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,8 +37,8 @@ plugins {
 
 allprojects {
   group = "com.gitlab.eclipse"
-  version = "0.7.0"
-  ext["bundleVersion"] = "0.7.0.${Instant.now().toEpochMilli()}"
+  version = "0.7.1"
+  ext["bundleVersion"] = "0.7.1.${Instant.now().toEpochMilli()}"
 
   repositories {
     gradlePluginPortal()
