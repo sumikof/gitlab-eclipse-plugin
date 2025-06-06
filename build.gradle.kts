d5474da70cd157c3f2875d6272e1e291797335f1
Jean-Gabriel Doyon <jgdoyon@gitlab.com>
2025-06-06 11:50:51 -0400

    chore: Prepare 0.7.3 snapshot

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,8 +37,8 @@ plugins {
 
 allprojects {
   group = "com.gitlab.eclipse"
-  version = "0.7.2"
-  ext["bundleVersion"] = "0.7.2.${Instant.now().toEpochMilli()}"
+  version = "0.7.3-SNAPSHOT"
+  ext["bundleVersion"] = "0.7.3.${Instant.now().toEpochMilli()}"
 
   repositories {
     gradlePluginPortal()
