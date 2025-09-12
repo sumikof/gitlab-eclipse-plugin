3ebbdabc6f55ddacf6217ccfd8636178650fffdc
Karl Jamoralin <21925675-kjamoralin@users.noreply.gitlab.com>
2025-09-12 13:24:32 -0700

    chore: Prepare for 0.7.8 snapshot

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,8 +37,8 @@ plugins {
 
 allprojects {
   group = "com.gitlab.eclipse"
-  version = "0.7.7"
-  ext["bundleVersion"] = "0.7.7.${Instant.now().toEpochMilli()}"
+  version = "0.7.8-SNAPSHOT"
+  ext["bundleVersion"] = "0.7.8.${Instant.now().toEpochMilli()}"
 
   repositories {
     gradlePluginPortal()
