ad4e3326e425afdb3b30fc15870f51274c9b1a0a
Karl Jamoralin <kjamoralin@gitlab.com>
2025-11-14 15:28:53 -0800

    Prepare 0.8.0 release

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,8 +37,8 @@ plugins {
 
 allprojects {
   group = "com.gitlab.eclipse"
-  version = "0.7.9-SNAPSHOT"
-  ext["bundleVersion"] = "0.7.9.${Instant.now().toEpochMilli()}"
+  version = "0.8.0"
+  ext["bundleVersion"] = "0.8.0.${Instant.now().toEpochMilli()}"
 
   repositories {
     gradlePluginPortal()
