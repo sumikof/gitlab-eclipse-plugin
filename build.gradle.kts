03b83c2095b69c42af53eefc4ecb3c548e1434d4
Jean-Gabriel Doyon <jgdoyon@gitlab.com>
2025-11-20 17:46:49 -0500

    chore: prepare 0.8.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,8 +37,8 @@ plugins {
 
 allprojects {
   group = "com.gitlab.eclipse"
-  version = "0.8.1"
-  ext["bundleVersion"] = "0.8.1.${Instant.now().toEpochMilli()}"
+  version = "0.8.2-SNAPSHOT"
+  ext["bundleVersion"] = "0.8.2.${Instant.now().toEpochMilli()}"
 
   repositories {
     gradlePluginPortal()
